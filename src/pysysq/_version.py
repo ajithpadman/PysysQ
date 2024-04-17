0103845d6b5c599adf39ff19601ea223b14e86b1
Ajith Padman <ajith.padman@gmail.com>
2024-04-17 08:51:11 +0200

    changed the generator statemachine

--- src/pysysq/_version.py
+++ src/pysysq/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.2.dev10+g8f5ac0d.d20240416'
-__version_tuple__ = version_tuple = (0, 0, 2, 'dev10', 'g8f5ac0d.d20240416')
+__version__ = version = '0.0.2.dev11+g1260cf0.d20240417'
+__version_tuple__ = version_tuple = (0, 0, 2, 'dev11', 'g1260cf0.d20240417')
--- src/pysysq/sq_base/sq_pkt_gen/states/sq_pkt_generator_queuing_state.py
+++ src/pysysq/sq_base/sq_pkt_gen/states/sq_pkt_generator_queuing_state.py
@@ -5,15 +5,15 @@ from ...sq_event import SQEvent
 class SQPktGeneratorQueuingState(SQPktGeneratorState):
     def process_packet(self, evt: SQEvent):
         if len(self.owner.packets)>0:
-            self.owner.output_q.push(self.owner.packets[self.owner.tick])
-            self.owner.generated_pkts += 1
-            self.owner.total_pkts += 1
-            self.owner.logger.info(f'Packet {self.owner.packets[self.owner.tick]} Ready for Queuing')
-            self.owner.tick += 1
-        if self.owner.tick >= len(self.owner.packets):
-            self.owner.packets = []
-            self.owner.tick = 0
-            self.owner.set_state(self.factory.create_state(name='GENERATING', owner=self.owner))
+            for pkt in self.owner.packets:
+                self.owner.output_q.push(pkt)
+                self.owner.generated_pkts += 1
+                self.owner.total_pkts += 1
+                self.owner.logger.info(f'Packet {pkt} Ready for Queuing')
+                self.owner.tick += 1
+        self.owner.packets = []
+        self.owner.tick = 0
+        self.owner.set_state(self.factory.create_state(name='GENERATING', owner=self.owner))
         self.owner.finish_indication()
 
     def get_state_name(self):
